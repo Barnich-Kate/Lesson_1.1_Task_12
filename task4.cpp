115466bc88599e7fa23ad4819f5c2cf5cca616b8
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-06-16 21:53:06 +0300

    Create task4.cpp
    Add the solution of the task

---
+++ task4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    float d, L;
+    const float Pi = 3.14;
+    cout<<"Type diametr: ";
+    cin >> d;
+    L = Pi * d;
+    cout << "The lenght of circle is equal: " << L << endl;
+
+    return 0;
+}
